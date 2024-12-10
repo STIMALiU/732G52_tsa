41bc520973176f921ef06966663d5ec57e727759
josefwilzen <josefwilzen@gmail.com>
2024-12-11 00:05:54 +0100

    updates

--- code/trucks_cv.R
+++ code/trucks_cv.R
@@ -1,4 +1,5 @@
 
+
 library(fpp3)
 
 trucks0<-read.csv("https://raw.githubusercontent.com/STIMALiU/732G52_tsa/refs/heads/main/data/NyLastbilregistreringar.csv")
@@ -11,10 +12,10 @@ trucks
 autoplot(trucks)
 
 
-trucks_cv_tr <- trucks |> stretch_tsibble(.init = 250, .step = 1)
+trucks_cv_tr <- trucks |> stretch_tsibble(.init = 280, .step = 1)
 
 model1_results <- trucks_cv_tr |>
-  model(model1=ARIMA(count~pdq(1,1,0)+PDQ(0,1,0))) |>
+  model(model1=ARIMA(count~pdq(1,1,1)+PDQ(0,1,0))) |>
   forecast(h = 12) |>
   group_by(.id) |>
   mutate(h = row_number()) |>
@@ -23,7 +24,7 @@ model1_results <- trucks_cv_tr |>
 
 
 model2_results <- trucks_cv_tr |>
-  model(model2=ARIMA(count~pdq(3,1,0)+PDQ(0,1,3))) |>
+  model(model2=ARIMA(count~pdq(3,1,0)+PDQ(0,1,1))) |>
   forecast(h = 12) |>
   group_by(.id) |>
   mutate(h = row_number()) |>
