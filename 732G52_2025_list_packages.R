e1f1279553bc5d3e6e89bb473cfbd0ba48d3452b
josefwilzen <josefwilzen@gmail.com>
2025-10-29 11:05:20 +0100

    list of R-packages used in the course 2025

---
+++ 732G52_2025_list_packages.R
@@ -0,0 +1,15 @@
+
+library(tidyverse)
+library(ggplot2)
+library(cowplot)
+library(lubridate)
+library(stringr)
+
+library(fpp3)
+library(tswge)
+library(TSA)
+
+library(ggfortify)
+library(lmtest)
+library(car)
+
