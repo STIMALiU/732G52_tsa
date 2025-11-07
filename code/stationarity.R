a6b7d488de6a8430971032b4c6108fe8b1258682
josefwilzen <josefwilzen@gmail.com>
2025-11-07 10:12:22 +0100

    updates

--- code/stationarity.R
+++ code/stationarity.R
@@ -37,7 +37,7 @@ set.seed(6435)
 y<-rnorm(n = nobs,mean = 0,sd = abs(rnorm(n = nobs,mean = scale(time_index)+1,sd = 0.01)))
 plot(time_index,y,t="l",ylim=c(-10,10))
 acf(y)
-# vi ser inte i acf-plotten varierar över tid
+# vi ser inte i acf-plotten att variansen/standardavvikelsen varierar över tid
 # här är det bättre att studera den vanliga tidserie-plotten
 # om vi har anpassat en modell till serien, då kan vi undersöka residualer mot tidsindex
 
@@ -47,6 +47,7 @@ set.seed(35)
 y<-10+x+0.1*x^2-2*x^3+rnorm(n=nobs,sd=0.5)
 plot(time_index,y,t="l")
 acf(y,lag.max = 40)
+acf(y,lag.max = 60)
 # här ser vi tydligt i acf-plotten att serien inte är väntevärde stationär
 
 
@@ -56,6 +57,10 @@ set.seed(85)
 e<-rnorm(n = nobs,mean = 0,sd = abs(rnorm(n = nobs,mean = 1.5*scale(time_index)+0.3,sd = 0.01)))
 y<-10+x+0.1*x^2-2*x^3+e
 plot(time_index,y,t="l")
+
+acf(y,lag.max = 40)
+# vi tar fram acf-plotten så kan vi se problemen med icke-konstant väntevärde,
+# men likt tidigare så ser vi inte icke-konstant varians i denna plot
 
 
 
@@ -75,7 +80,7 @@ y0<-1
 x_val<-seq(-2,2,length=N)
 # serien skapas med en AR(1) modell där AR-parametern ändras över tid
 # mer om detta senare
-ar1_para_vect<- -tanh(4*x_val)
+ar1_para_vect<- -tanh(3*x_val)
 set.seed(71)
 y<-rep(0,N)
 for(i in 1:N){
@@ -98,12 +103,13 @@ acf(y[1:100])
 # autokorrelation i slutet av serien:
 acf(y[201:300])
 
-# autokorrelation i slutet av serien:
+# autokorrelation för hela serien:
 acf(y)
 
 # detta är ett (något överdrivet) exempel när autokorrelation ändras över tid
 # detta innebär att autokorrelation inte bara beror på det relativa tidsavståndet
 # mellan tidpunkter, utan även var i tiden som man undersöker.
+
 
 
 #-------------------------------------------------------------------------------
@@ -121,18 +127,20 @@ x<-(1:nobs)
 sd_val<-4
 set.seed(76)
 # vi simulerar e (slumptermen) så att den inte är oberoende, från en AR(1)-modell
+# mer om AR-modeller senare i kursen
 # y_t = rho*y_(t-1) + error
 set.seed(35)
 # testa båda varianterna av e nedan:
-e<-arima.sim(n = nobs,list(ar=c(0.9)),rand.gen = rnorm,sd=1)
-e<-arima.sim(n = nobs,list(ar=c(0.7)),rand.gen = rnorm,sd=1)
+e<-arima.sim(n = nobs,list(ar=c(0.9)),rand.gen = rnorm,sd=1) # ar=0.9 
+e<-arima.sim(n = nobs,list(ar=c(0.7)),rand.gen = rnorm,sd=1) # ar=0.7
 plot(x,e,t="l")
 
 # SAC
 acf(e)
+# undersöker korrelationen "just nu" med en lags förskjutning:
 e_current<-e[2:300]
 e_lag1<-e[1:299]
-plot(e_lag1,e_current)
+plot(e_lag1,e_current) # plotten visar om det finns någon lag1-autokorrelation
 
 cor(e_lag1,e_current)
 
@@ -156,20 +164,25 @@ head(e_pacf$lag)
 
 #-------------------------------------------------------------------------------
 # fall 2
+
+# vi simulerar e (slumptermen) så att den inte är oberoende, från en AR(1)-modell
+# mer om AR-modeller senare i kursen
 set.seed(36)
 e<-arima.sim(n = nobs,list(ar= -0.8),rand.gen = rnorm,sd=1)
 
 plot(x,e,t="l")
+# undersöker korrelationen "just nu" med en lags förskjutning:
 e_current<-e[2:300]
 e_lag1<-e[1:299]
-plot(e_lag1,e_current)
+plot(e_lag1,e_current) # plotten visar om det finns någon lag1-autokorrelation
 
 cor(e_lag1,e_current)
 
 acf(e)
 #?acf
 acf(e,lag.max = 40)
-acf(e,lag.max = 30,type = "cov")
+
+acf(e,lag.max = 30,type = "cov") # autokovariansen
 
 
 
